7b9bd113d5f30cfc591b8f7ebfff8fc5c090f3b9
iness-1810 <ines.sebastian.santamaria@gmail.com>
2025-06-21 14:08:44 +0200

    add

---
+++ assets/js/main.js
@@ -0,0 +1,78 @@
+
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			normal:    [ '1081px',  '1280px'  ],
+			narrow:    [ '821px',   '1080px'  ],
+			narrower:  [ '737px',   '820px'   ],
+			mobile:    [ '481px',   '736px'   ],
+			mobilep:   [ null,      '480px'   ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			mode: 'fade',
+			speed: 300,
+			alignment: 'center',
+			noOpenerFade: true
+		});
+
+	// Nav.
+
+		// Button.
+			$(
+				'<div id="navButton">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						'<a href="index.html" class="link depth-0">Home</a>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'top',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
+				    // Cargar header global
+				$(function() {
+					$("#header-placeholder").load("header.html", function() {
+						// Re-inicializar el menú después de cargar el header
+						if (typeof $.fn.dropotron !== 'undefined') {
+							$('#nav > ul').dropotron({
+								mode: 'fade',
+								speed: 300,
+								alignment: 'center',
+								noOpenerFade: true
+							});
+						}
+					});
+				});
+				 $(function(){   
+					$("#footer-placeholder").load("footer.html");
+        		});
+})(jQuery);
